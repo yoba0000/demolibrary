2d8384d7b892d5b22174637bda9d5daa170550cb
Youssef Bazine <baya0000@gmail.com>
2021-04-28 19:50:58 +0200

    Test

---
+++ src/test/java/com/baya/demolibrary/AppTest.java
@@ -0,0 +1,21 @@
+package com.baya.demolibrary;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
